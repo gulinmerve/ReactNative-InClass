a9d9de6793524d49c0b7e52c380ae8dca6ef5b17
Merve <gulineyel@gmail.com>
2020-10-08 20:13:18 +0300

    class started

--- App.js
+++ App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import {SafeAreaView, View, Text, StyleSheet} from 'react-native';
 
-
-
-const App = ()  => {
+const App = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        
-      </SafeAreaView>
-    </>
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.container}>
+        <Text style={[styles.text, styles.text_2]}>Clarusway</Text>
+        <Text style={styles.text}>Components</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
-
-
 export default App;
+
+const styles = StyleSheet.create({
+  container: {flex: 1, justifyContent: 'center', alignItems: 'center'},
+  text: {fontSize: 40, fontWeight: 'bold'},
+  text_2: {color: 'rgb(170,71,126)', fontSize: 35}
+});
