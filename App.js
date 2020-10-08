d32434fac8f80a89b61fa36925ccdaee178b0657
Merve <gulineyel@gmail.com>
2020-10-08 18:45:44 +0300

    08-10-20 started

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+
+
+const App = ()  => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        
+      </SafeAreaView>
+    </>
+  );
+};
+
+
+
+export default App;
