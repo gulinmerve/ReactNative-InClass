e1fdf335c7b3b5f035fe6e73eb0d47f1ded37c21
Merve <gulineyel@gmail.com>
2020-10-13 20:16:15 +0300

    lecture started

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+
+
+const App = () => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View>
+      
+      </View>
+    </SafeAreaView>
+  );
+};
+export default App;
