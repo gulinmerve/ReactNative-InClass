47db30615a2cbd3f8e5087e66b6a839e10a99838
Merve <gulineyel@gmail.com>
2020-10-13 22:49:27 +0300

    class changed

--- src/Class.js
+++ src/Class.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import { SafeAreaView, View, Text } from 'react-native';
+import { SafeAreaView, View, Text, Button } from 'react-native';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            counter: 0
+        }
     }
     render(){
         return(
             <SafeAreaView>
-                <Text>Merhaba Full Stack</Text>
-            </SafeAreaView>
+            <View>
+              <Text style={{ fontSize: 250}}>{this.state.counter}</Text>
+      
+              <Button 
+                  title="Up!"
+                  onPress={ () => this.setState(
+                      {
+                          counter: this.state.counter + 1
+                      }
+                  )}
+              />
+            </View>
+          </SafeAreaView>
         )
     }
 }
