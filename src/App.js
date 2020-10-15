a5855c37fb73f993e6c51d4d06b1e24e2bdb4390
Merve <gulineyel@gmail.com>
2020-10-15 20:18:10 +0300

    app added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+export default App;
