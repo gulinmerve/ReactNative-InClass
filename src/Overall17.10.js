f74a08ebb1fab988f21b1345b142ecab43adfd3a
Merve <gulineyel@gmail.com>
2020-10-17 18:21:47 +0300

    panel added

--- src/Overall17.10.js
+++ src/Overall17.10.js
@@ -1,39 +1,20 @@
 // Overall - 1
 import React from 'react';
-import {
-  SafeAreaView,
-  View,
-  Text,
-  TouchableOpacity,
-  StyleSheet,
-} from 'react-native';
+import { SafeAreaView, View, Text, Alert } from 'react-native';
 
-import Button from './components/Button';
+import InputPanel from './components/InputPanel';
 
 const App = () => {
-  return (
-    <SafeAreaView>
-      <View>
-        <Text style={{fontSize: 50}}>Hello World</Text>
-      </View>
+    return (
+        <SafeAreaView>
+            <View>
+                <Text style={{ fontSize: 50 }}>Hello World</Text>
+            </View>
 
-      <Button
-        users={[1, 2, 3, 4, 5]}
-        count={189}
-        number={3}
-        color="#81d4fa"
-        banner="Press"
-      />
+            <InputPanel />
 
-      <Button
-        users={[1, 2, 3, 4, 5]}
-        count={189}
-        number={3}
-        // color="#81d4fa"
-        banner="Press"
-      />
-    </SafeAreaView>
-  );
-};
+        </SafeAreaView>
+    )
+}
 
 export default App;
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,33 +1,34 @@
-import React from 'react';
-import {Text, TouchableOpacity, StyleSheet} from 'react-native';
+// import React from 'react';
+// import {Text, TouchableOpacity, StyleSheet} from 'react-native';
 
-const Button = (props) => {
-  // let selectedColor = props.color;
-  // if(props.color === undefined)
-  //     selectedColor = "pink"
+// const Button = (props) => {
+//   // let selectedColor = props.color;
+//   // if(props.color === undefined)
+//   //     selectedColor = "pink"
 
-  return (
-    <TouchableOpacity
-      style={[
-        myStyles.buttonContainer,
+//   return (
+//     <TouchableOpacity
+//       style={[
+//         myStyles.buttonContainer,
 
-        {
-          backgroundColor: props.color === undefined ? 'pink' : props.color,
-        },
-      ]}>
-      <Text>{props.banner}</Text>
-    </TouchableOpacity>
-  );
-};
+//         {
+//           backgroundColor: props.color === undefined ? 'pink' : props.color,
+//         },
+//       ]}
+//       onPress={props.sayingHello}>
+//       <Text>{props.banner}</Text>
+//     </TouchableOpacity>
+//   );
+// };
 
-export default Button;
+// export default Button;
 
-const myStyles = StyleSheet.create({
-  buttonContainer: {
-    backgroundColor: '#a5d6a7',
-    padding: 15,
-    margin: 10,
-    borderRadius: 5,
-    alignItems: 'center',
-  },
-});
+// const myStyles = StyleSheet.create({
+//   buttonContainer: {
+//     backgroundColor: '#a5d6a7',
+//     padding: 15,
+//     margin: 10,
+//     borderRadius: 5,
+//     alignItems: 'center',
+//   },
+// });
