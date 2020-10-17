978b8dd9dd0ea753babdabaab426842843a4ddfc
Merve <gulineyel@gmail.com>
2020-10-17 17:22:35 +0300

    ternary control

--- src/Overall17.10.js
+++ src/Overall17.10.js
@@ -1,22 +1,39 @@
 // Overall - 1
 import React from 'react';
-import { SafeAreaView, View, Text, Button, TouchableOpacity, StyleSheet } from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
 
-import MyButton from './components/Button';
+import Button from './components/Button';
 
 const App = () => {
-    return (
-        <SafeAreaView>
-            <View>
-                <Text style={{ fontSize: 50 }}>Hello World</Text>
-            </View>
+  return (
+    <SafeAreaView>
+      <View>
+        <Text style={{fontSize: 50}}>Hello World</Text>
+      </View>
 
-            <MyButton />
-            <MyButton />
-            <MyButton />
+      <Button
+        users={[1, 2, 3, 4, 5]}
+        count={189}
+        number={3}
+        color="#81d4fa"
+        banner="Press"
+      />
 
-        </SafeAreaView>
-    )
-}
+      <Button
+        users={[1, 2, 3, 4, 5]}
+        count={189}
+        number={3}
+        // color="#81d4fa"
+        banner="Press"
+      />
+    </SafeAreaView>
+  );
+};
 
 export default App;
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,22 +1,33 @@
 import React from 'react';
-import { Text, TouchableOpacity, StyleSheet } from 'react-native';
+import {Text, TouchableOpacity, StyleSheet} from 'react-native';
 
-const Button = () => {
-    return (
-        <TouchableOpacity style={myStyles.buttonContainer}>
-            <Text>Press me!</Text>
-        </TouchableOpacity>
-    )
-}
+const Button = (props) => {
+  // let selectedColor = props.color;
+  // if(props.color === undefined)
+  //     selectedColor = "pink"
+
+  return (
+    <TouchableOpacity
+      style={[
+        myStyles.buttonContainer,
+
+        {
+          backgroundColor: props.color === undefined ? 'pink' : props.color,
+        },
+      ]}>
+      <Text>{props.banner}</Text>
+    </TouchableOpacity>
+  );
+};
 
 export default Button;
 
 const myStyles = StyleSheet.create({
-    buttonContainer: {
-        backgroundColor: '#a5d6a7',
-        padding: 15,
-        margin: 10,
-        borderRadius: 5,
-        alignItems: 'center'
-    }
-})
+  buttonContainer: {
+    backgroundColor: '#a5d6a7',
+    padding: 15,
+    margin: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+});
