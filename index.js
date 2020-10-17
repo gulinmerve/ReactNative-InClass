40ef900a6682006d41908a6a60486706dfff1fdb
Merve <gulineyel@gmail.com>
2020-10-17 16:07:51 +0300

    Overall 17.10 started

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './src/Functional';
+import App from './src/Overall17.10';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
